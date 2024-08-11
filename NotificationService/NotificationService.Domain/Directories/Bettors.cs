2d968475813d47c6f24f178807e4d43d6935b2ba
skorodumov.ms <skorodumov.ms@uc-osnovanie.ru>
2024-08-11 11:45:14 +0300

    added DTO for Directories

---
+++ NotificationService/NotificationService.Domain/Directories/Bettors.cs
@@ -0,0 +1,20 @@
+﻿using NotificationService.Domain.Abstractions.DTO;
+
+namespace NotificationService.Domain.Directories
+{
+    /// <summary>
+    /// Справочник игроков
+    /// </summary>
+    public sealed class Bettors : LaterDeletedEntity
+    {
+        /// <summary>
+        /// Как обращаться к игроку
+        /// </summary>
+        public string Nickname { get; set; }
+
+        /// <summary>
+        /// Доступные способы связи с игроком
+        /// </summary>
+        public List<BettorAddresses> BettorMessangers { get; } = [];
+    }
+}
