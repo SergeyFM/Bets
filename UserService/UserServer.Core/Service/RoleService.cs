92ba74ec727292d7b08f0da9b0bdcdb117c8242f
zinovkin_ia <lakki020896@gmail.com>
2024-09-16 21:22:59 +0300

    Добавлена работа с ролями пользователя

---
+++ UserService/UserServer.Core/Service/RoleService.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using UserServer.Core.DTO;
+using UserServer.Core.Entities;
+using UserServer.Core.Interfaces;
+
+namespace UserServer.Core.Service
+{
+    public class RoleService : IRoleService
+    {
+        private readonly RoleManager<Role> _roleManager;
+        private readonly UserManager<User> _userManager;
+
+        public RoleService(RoleManager<Role> roleManager, UserManager<User> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public async Task<IEnumerable<RoleDto>> GetAllRolesAsync()
+        {
+            
+            return await _roleManager.Roles.Select(x => new RoleDto 
+            {
+                ID = x.Id,
+                Name = x.Name
+            }).ToListAsync();
+        }
+
+        public async Task<RoleDto> CreateRoleAsync(string roleName)
+        {
+            var role = new Role { Name = roleName };
+            var result = await _roleManager.CreateAsync(role);
+
+            if (result.Succeeded)
+            {
+                return new RoleDto { ID = role.Id, Name = role.Name };
+            }
+
+            throw new Exception($"Failed to create role: {string.Join(", ", result.Errors.Select(e => e.Description))}");
+        }
+
+        public async Task<IdentityResult> AddUserToRoleAsync(string userId, RoleDto role)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null) throw new Exception($"User not found.");
+
+            var result = await _userManager.AddToRoleAsync(user, role.Name);
+
+            return result;
+        }
+
+        public async Task<IdentityResult> RemoveUserFromRoleAsync(string userId, RoleDto role)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null) throw new Exception("User not found.");
+
+            var result = await _userManager.RemoveFromRoleAsync(user, role.Name);
+
+            return result;
+        }
+
+        public async Task<IEnumerable<string>> GetUserRolesAsync(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null) throw new Exception("User not found.");
+
+            return await _userManager.GetRolesAsync(user);
+        }
+
+        public async Task<IEnumerable<UserDto>> GetUsersInRoleAsync(string roleName)
+        {
+            var usersInRole = await _userManager.GetUsersInRoleAsync(roleName);
+
+            return usersInRole.Select(u => new UserDto
+            {
+                Email = u.Email,
+                FirstName = u.FirstName,
+                LastName = u.LastName,
+                UserName = u.UserName
+            }).ToList();
+        }
+    }
+}
---
+++ UserService/UserServer/Controllers/V1/RolesController.cs
@@ -0,0 +1,78 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UserServer.Core.DTO;
+using UserServer.Core.Interfaces;
+
+namespace UserServer.WebHost.Controllers.V1
+{
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Admin")]
+    public class RolesController : ControllerBase
+    {
+        private readonly IRoleService _roleService;
+
+        public RolesController(IRoleService roleService)
+        {
+            _roleService = roleService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<RoleDto>>> GetRoles()
+        {
+            var roles = await _roleService.GetAllRolesAsync();
+            return Ok(roles);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<RoleDto>> CreateRole(string roleName)
+        {
+            var createdRole = await _roleService.CreateRoleAsync(roleName);
+            return CreatedAtAction(nameof(GetRoles), createdRole);
+        }
+
+        [HttpPost("{userId}")]
+        public async Task<IActionResult> AddUserToRole(string userId, RoleDto role)
+        {
+            var result = await _roleService.AddUserToRoleAsync(userId, role);
+            if (result.Succeeded) 
+            {
+                return NoContent();
+            }
+
+            return BadRequest($"Failed to add user to role: {string.Join(", ", result.Errors.Select(e => e.Description))}");
+        }
+
+        [HttpDelete("{userId}&roles={roleName}")]
+        public async Task<IActionResult> RemoveUserFromRole(string userId, RoleDto role)
+        {
+            var result = await _roleService.RemoveUserFromRoleAsync(userId, role);
+            if (result.Succeeded) 
+            {
+                return NoContent();
+            }
+
+            return BadRequest($"Failed to remove user from role: {string.Join(", ", result.Errors.Select(e => e.Description))}");
+        }
+
+        // Получение ролей пользователя
+        [HttpGet("{userId}/roles")]
+        public async Task<ActionResult<IEnumerable<string>>> GetUserRoles(string userId)
+        {
+            var roles = await _roleService.GetUserRolesAsync(userId);
+
+            return Ok(roles);
+        }
+
+        // Получение пользователей в роли
+        [HttpGet("users-in-role/{roleName}")]
+        public async Task<ActionResult<IEnumerable<UserDto>>> GetUsersInRole(string roleName)
+        {
+            var users = await _roleService.GetUsersInRoleAsync(roleName);
+
+            return Ok(users);
+        }
+    }
+}
